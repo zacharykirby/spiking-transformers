3407eb6ff2153c71af882539a9ced58cebd5aac5
Zach <zkirby@live.com>
2025-12-21 20:20:57 -0600

    getting started with Karpathy 'Let's build GPT'

---
+++ nanoGPT/dev.py
@@ -0,0 +1,145 @@
+# this should've been a python notebook, oh well
+
+# read shakespeare
+with open('./data/input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+print('char length of dataset: ', len(text))
+
+# Sample text
+print(text[:1000])
+
+# build vocabulary
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print('vocab: ' + ''.join(chars))
+print(vocab_size)
+
+# build encoder/decoder
+# Google uses (kinda) SentencePiece, OpenAI uses (kinda) tiktoken
+stoi = {ch:i for i,ch in enumerate(chars)}
+itos = {i:ch for i,ch in enumerate(chars)}
+
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output list of integers
+decode = lambda l: ''.join(itos[i] for i in l) # decoder: take a list of integers, output a string
+
+print(encode('hi there!'))
+print(decode(encode('hi there!')))
+
+# import tiktoken
+# enc = tiktoken.get_encoding('gpt2')
+# print('gpt2 vocab size:', enc.n_vocab)
+
+# encode the ENTIRE text dataset into a torch.Tensor
+import torch
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype) # torch.Size([1115394]), torch.int64
+print(data[:1000])
+
+# split data to train/val
+n = int(0.9*len(data)) # first 90% will train
+train_data = data[:n]
+val_data = data[n:]
+
+# also called context length, chunk size, etc
+block_size = 8
+print(train_data[:block_size+1])
+
+# chunks...overlap...kind of?
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print(f'when input is {context} the target: {target}')
+
+
+# batch (becuase chunks are small and we can process so many)
+torch.manual_seed(1337)
+batch_size = 4 # how many parallel processes?
+block_size = 8 # what is the maximum context length?
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape) # torch.Size([4, 8])
+print(xb)
+print('targets:')
+print(yb.shape) # torch.Size([4, 8])
+print(yb)
+print('*'*50)
+
+for b in range(batch_size): # batch dimension
+    for t in range(block_size): # time dimension
+        context = xb[b, :t+1]
+        target = yb[b,t]
+        print(f'when input is {context.tolist()} the target: {target}')
+
+# simplest neural network
+# bigram langugage model
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337)
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off logits for the next token from lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C), or (Batch, Time (Block), Channel (Vocab_Size))
+
+        if targets is None:
+            loss = None
+        else:
+            # negative log loss
+            # loss = F.cross_entropy(logits, targets) # https://docs.pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+            # BASICALLY...cross entropy needs channels in second dim, let's reshape
+            
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T) # or, .view(-1)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is (B,T) array of indices in current context
+        for _ in range(max_new_tokens):
+            # get predictions
+            logits, loss = self(idx)
+
+            # focus only on last time step
+            logits = logits[:,-1,:] # becomes (B,C)
+
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B,C)
+
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B,1)
+
+            # concat sampled index to running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B,T+1)
+
+        return idx
+
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape) # torch.Size([4, 8, 65]) ORIGNALLY, then torch.Size([32, 65]) when reshaped
+print(loss) # -ln(1/65) = 4.8786
+
+# a zero object, just new line
+idx = torch.zeros((1,1), dtype=torch.long)
+print(decode(m.generate(idx,max_new_tokens=100)[0].tolist()))
